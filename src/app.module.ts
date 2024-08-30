6b65dc3a3e34548629047401b77d891436074685
Eric O Okiti <osivwiokiti@gmail.com>
2024-08-30 05:27:22 +0100

    config - add pipe validation - add swagger for api docs - add db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
+      synchronize: true, // Use this in development; disable in production
+      logging: true,
+    }),
+    VehiclesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Automatically remove properties that do not have any decorators
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are found
+      transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Autochek')
+    .setDescription('Autocheck API description')
+    .setVersion('1.0')
+    .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
